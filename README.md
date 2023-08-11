bae978e6bed0c9caf8dd9166223adfc9ecb40225
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-08-11 06:36:17 +0600

    add: User Model

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ npm install && npm start
 1. bootstrap - done
 2. Add dummy controllers - done
 3. Add Routes - done
-4. Check if routes work
-5. Connect db works
-6. get the front & connect with front
+4. Check if routes work - done
+5. Connect db works - done
+6. Create user model - done
+
+   - Email validation :
+
+   ```regex
+   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+   ```
+
+   - Add unique indexing for email
+
+7. get the front & connect with front
---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'must provide name'],
+    trim: true,
+    maxlength: [20, 'name cannot be more than 20 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'must provide email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    // creates unique index , but not a validator
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'must provide password'],
+    trim: true,
+    minlength: [6, 'name cannot be less than 20 characters'],
+  },
+})
+
+module.exports = mongoose.model('User', UserSchema)
