89f7b94230aa44ae46013502bfe62d494fc649b1
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-08-12 06:56:06 +0600

    fix: add bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -27,7 +28,6 @@ const UserSchema = new mongoose.Schema({
 
 // what do we want to accomplish before we save
 UserSchema.pre('save', async function () {
-  // generate random bytes
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
 })
