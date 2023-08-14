cb8f3009265363afc1743378fcdd6596213dca68
ayazhankadessova <kadessovaayazhan@gmail.com>
2023-08-14 08:15:50 +0600

    add: cast error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -21,6 +21,10 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.statusCode = 400
   }
 
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`
+    customError.statusCode = 400
+  }
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
